fe25ea73ca282ea56d6ab5e3e25e36b1d9a2830b
prakhar-kt <rahkarp@gmail.com>
2025-08-17 12:47:18 +0530

    add user model and schema

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
-def main():
-    print("Hello from textcase-pro!")
+from fastapi import FastAPI
+from contextlib import asynccontextmanager
+from app.db.config import create_tables
+from app.account.routers import router as account_router
 
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    await create_tables()
+    yield
 
-if __name__ == "__main__":
-    main()
+app = FastAPI(lifespan=lifespan)
+
+app.include_router(account_router)
